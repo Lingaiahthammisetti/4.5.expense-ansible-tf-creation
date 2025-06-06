b27eb9ad6f40789fb7c9ea48f8fc226cf8e71c3f
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-06-06 11:35:35 +0530

    expense ansible tf ec2

--- ansible_install.sh
+++ ansible_install.sh
@@ -3,6 +3,6 @@ dnf install ansible -y
 cd /tmp
 git clone https://github.com/Lingaiahthammisetti/4.4.expense-ansible.git
 cd 4.4.expense-ansible
-ansible-playbook -i inventory.ini mysql.yaml
+ansible-playbook -i inventory.ini db.yaml -e 'mysql_root_password=ExpenseApp@1'
 ansible-playbook -i inventory.ini backend.yaml
 ansible-playbook -i inventory.ini frontend.yaml
